aae4b4ccc2a3287cc1e8ebb756ddbd50ec79c65b
Tina <tina.1130@outlook.com>
2020-12-09 21:51:32 +0800

    新增join方法

--- miao/lodash/tina_record-lodash.js
+++ miao/lodash/tina_record-lodash.js
@@ -1,20 +1,39 @@
-var tina = function () {
+var tina_record_lodash = (function () {
   function compact(ary) {
-    let result = []
+    let result = [];
     for (let i = 0; i < ary.length; i++) {
       if (ary[i]) {
-        result.push(ary[i])
+        result.push(ary[i]);
       }
     }
-    return result
+    return result;
   }
 
   function add(augend, addend) {
     return augend + addend;
   }
 
+  function sum(array) {
+    let result = 0;
+    for (let i = 0; i < array.length; i++) {
+      result += array[i];
+    }
+    return result;
+  }
+
+  function join(array, string) {
+    let str = String(string);
+    let result = "";
+    for (let i = 0; i < array.length - 1; i++) {
+      result += array[i] + str;
+    }
+    return (result += array[array.length - 1]);
+  }
+
   return {
     compact,
     add,
-  }
-}()
+    sum,
+    join,
+  };
+})();
