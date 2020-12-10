4931e853efae723d74da2c686b09c377126fcd16
Tina <tina.1130@outlook.com>
2020-12-10 15:54:15 +0800

    新增join,indexOf,lastIndexOf,slice方法

--- miao/lodash/tina_record-lodash.js
+++ miao/lodash/tina_record-lodash.js
@@ -21,9 +21,47 @@ var tina_record = (function () {
     return result;
   }
 
+  function join(array, string) {
+    let str = String(string);
+    let result = "";
+    for (let i = 0; i < array.length - 1; i++) {
+      result += array[i] + str;
+    }
+    return (result += array[array.length - 1]);
+  }
+
+  function indexOf(array, valule, startIndex = 0) {
+    for (let i = startIndex; i < array.length; i++) {
+      if (array[i] == valule) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  function lastIndexOf(array, valule, startIndex = array.length - 1) {
+    for (let i = startIndex; i >= 0; i--) {
+      if (array[i] == valule) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  function slice(array, startIndex, endIndex) {
+    let result = [];
+    for (let i = startIndex; i < endIndex; i++) {
+      result.push(array[i]);
+    }
+    return result;
+  }
+
   return {
     compact,
     add,
     sum,
+    join,
+    indexOf,
+    lastIndexOf,
+    slice,
   };
 })();
